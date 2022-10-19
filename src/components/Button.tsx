34e8dc7b4aaf5374b140abb00ffde9f9def8206f
Sigurd Holmen <sigurd.holm1@gmail.com>
2022-10-19 12:11:19 +0200

    Button example

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { styled } from 'styled-components';
+
+const ButtonBase: FC = () => {
+	// implement this
+	return <button />
+}
+
+export const Button = styled(Button)`
+
+`;
