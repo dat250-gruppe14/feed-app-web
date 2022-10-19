db99f6d5513a43e92b075cdf26f22a56ab06508e
andersbuanes <anderstbuanes@hotmail.com>
2022-10-19 11:12:45 +0200

    fix react-query-devtools import fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import ErrorBoundary from './routes/components/ErrorBoundary';
 import { Routes } from './routes/Routes';
