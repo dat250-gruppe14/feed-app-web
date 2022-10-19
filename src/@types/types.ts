1484e3b4fafaf893a061300610fc63afb9d449d0
andersbuanes <anderstbuanes@hotmail.com>
2022-10-19 11:33:42 +0200

    add eslint indent and bracket spacing

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,55 +1,55 @@
 export enum PollAccess {
-    Public = "Public",
-    Private = "Private",
+  Public = "Public",
+  Private = "Private",
 }
 
 export enum AccountRole {
-    User = "User",
-    Admin = "Admin",
+  User = "User",
+  Admin = "Admin",
 }
 
 export interface User {
-    id: string;
-    name: string;
-    role: AccountRole;
+  id: string;
+  name: string;
+  role: AccountRole;
 }
 
 export interface Device {
-    id: string;
-    name: string;
-    pincode: number;
+  id: string;
+  name: string;
+  pincode: number;
 }
 
 export enum Answer {
-    NONE,
-    ANSWER_A,
-    ANSWER_B
+  NONE,
+  ANSWER_A,
+  ANSWER_B
 }
 
 export interface Poll {
-    id: string;
-    question: string;
-    optionOne: string;
-    optionTwo: string;
-    owner: User;
-    optionOneCount: number;
-    optionTwoCount: number;
-    startDate: Date;
-    endDate: Date;
-    access: PollAccess;
-    isClosed: boolean;
-    userAnswer: Answer;
+  id: string;
+  question: string;
+  optionOne: string;
+  optionTwo: string;
+  owner: User;
+  optionOneCount: number;
+  optionTwoCount: number;
+  startDate: Date;
+  endDate: Date;
+  access: PollAccess;
+  isClosed: boolean;
+  userAnswer: Answer;
 }
 
 export interface Vote {
-    id: string;
-    option: Answer;
-    poll: Poll;
+  id: string;
+  option: Answer;
+  poll: Poll;
 }
 
 export interface DeviceVote {
-    id: string;
-    answerACount: number;
-    answerBCount: number;
-    poll: Poll;
+  id: string;
+  answerACount: number;
+  answerBCount: number;
+  poll: Poll;
 }
