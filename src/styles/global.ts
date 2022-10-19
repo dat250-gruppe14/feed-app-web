d75103454606ac3aea224135f51704f4532e668a
andersbuanes <anderstbuanes@hotmail.com>
2022-10-19 12:21:43 +0200

    Add default global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
+import { colors } from './colors'
 
 export default createGlobalStyle`
   * {
+    background: ${colors.backgroundPrimary};
+    box-sizing: border-box;
+    color: white;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
     outline: 0;
   }
 
