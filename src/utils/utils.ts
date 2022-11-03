fb54d5b885ec97f1edef9135161bd9645446c72b
Rune Mæstad <54098894+rune-m@users.noreply.github.com>
2022-11-03 13:55:02 +0100

    Fixed NaN bug on poll card. Some styling (#15)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,14 +13,21 @@ export const mapRouteToHeaderTitle = (path: string) => {
   return '';
 };
 
-export const calculatePercentage = (value: number, total: number): number => (value / total * 100);
+export const calculatePercentage = (value: number, total: number): number => {
+  if (total === 0) return 0;
+  return (value / total) * 100;
+};
 
-export const getRemainingPollDays = (endDate: Date | undefined): number | undefined => {
+export const getRemainingPollDays = (
+  endDate: Date | undefined,
+): number | undefined => {
   const msPerDay = 24 * 60 * 60 * 1000;
 
   const now = new Date();
 
-  const difference = endDate ? Math.round(Math.abs(Number(endDate)- Number(now)) / msPerDay) : undefined;
+  const difference = endDate
+    ? Math.round(Math.abs(Number(endDate) - Number(now)) / msPerDay)
+    : undefined;
 
   return difference;
 };
